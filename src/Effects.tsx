091ac09f0b177c990f0bf8cc2acdbf0ca80b7c12
GrigoriyKostryukov <kostryukov-grigoriy@mail.ru>
2022-12-05 20:24:27 +0300

    Resolve task

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,5 +1,21 @@
 import { subscribe, unsubscribe } from './resources/API';
+import { useState, useEffect } from 'react';
 
-export function Effects(props: { sourceId: string }) {
-    return <div>123</div>;
+export function Effects(props: { sourceId: string }): any {
+    const [message, setMessage] = useState(-1);
+    const updateMessage = (message: any) => {
+        setMessage(message);
+    };
+
+    useEffect(() => {
+        setMessage(-1);
+        subscribe(props.sourceId, updateMessage);
+        return () => unsubscribe(props.sourceId, updateMessage);
+    }, [props.sourceId]);
+
+    return (
+        <div>
+            {props.sourceId}: {message}
+        </div>
+    );
 }
